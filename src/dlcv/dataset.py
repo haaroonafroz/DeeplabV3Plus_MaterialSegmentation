c9174f6f6c03f9f9757a06ae5d1eb6026be8743b
haaaroon.afroz <haaroon.afroz.ognawala@uni-weimar.de>
2024-08-07 15:47:56 +0200

    import statement

--- src/dlcv/dataset.py
+++ src/dlcv/dataset.py
@@ -4,6 +4,7 @@ from PIL import Image
 import torch
 from torch.utils.data import Dataset
 import numpy as np
+import torch.nn.functional as F
 
 class CustomSegmentationDataset(Dataset):
     def __init__(self, material_root, transform=None, target_transform=None, mode='train', downsample_size=2):
