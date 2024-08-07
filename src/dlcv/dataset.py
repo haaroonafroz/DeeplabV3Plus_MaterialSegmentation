4c8f9ad3f4c23b7c43c81964bf8d1be8b78aa1f5
haaaroon.afroz <haaroon.afroz.ognawala@uni-weimar.de>
2024-08-07 15:43:03 +0200

    downsample max pool

--- src/dlcv/dataset.py
+++ src/dlcv/dataset.py
@@ -5,11 +5,12 @@ import torch
 from torch.utils.data import Dataset
 
 class CustomSegmentationDataset(Dataset):
-    def __init__(self, material_root, transform=None, target_transform=None, mode='train'):
+    def __init__(self, material_root, transform=None, target_transform=None, mode='train', downsample_size=2):
         self.material_root = material_root
         self.transform = transform
         self.target_transform = target_transform
         self.mode = mode
+        self.downsample_size = downsample_size
 
         # Load Material dataset annotations
         with open(os.path.join(material_root, 'annotations.json'), 'r') as f:
@@ -38,11 +39,20 @@ class CustomSegmentationDataset(Dataset):
         binary_mask = binary_mask.resize((256,256), Image.NEAREST)
         class_mask = class_mask.resize((256,256), Image.NEAREST)
 
+        # Downsample the class mask using max pooling
+        class_mask_tensor = torch.from_numpy(np.array(class_mask)).float()
+        class_mask_downsampled = max_pooling_downsample(class_mask_tensor, self.downsample_size)
+
         # Apply the appropriate transforms
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             binary_mask = self.target_transform(binary_mask)
-            class_mask = self.target_transform(class_mask)
+            class_mask_downsampled = self.target_transform(class_mask_downsampled)
 
-        return image, binary_mask, class_mask
+        return image, binary_mask, class_mask_downsampled
+    
+def max_pooling_downsample(class_mask, pool_size):
+    class_mask = class_mask.unsqueeze(0).unsqueeze(0)  # Add batch and channel dimensions
+    downsampled_mask = F.max_pool2d(class_mask, kernel_size=pool_size, stride=pool_size)
+    return downsampled_mask.squeeze(0).squeeze(0)  # Remove batch and channel dimensions
